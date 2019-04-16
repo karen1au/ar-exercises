d84ad482425c5ee573c603e3eadcb6332b0ddf17
karen1au <kagemikaka@hotmail.com>
2019-04-16 18:00:53 +0000

    exercise 7, added validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+store_name = $stdin.gets.chomp
+store = Store.create(name: store_name)
+store.errors.full_messages.each do |e|
+  puts e
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
-  has_many :employees
+  has_many :employees, inverse_of: :store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :carry_apparel
+  
+  def carry_apparel
+    if !mens_apparel && !womens_apparel
+      errors[:base] << "must carry at least one apparel"
+    end
+  end
 end
