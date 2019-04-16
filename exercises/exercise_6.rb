152dc3db2833c9439b18841cd4c3003d8872b2c6
karen1au <kagemikaka@hotmail.com>
2019-04-16 16:38:09 +0000

    exercise 6, create employees referencing to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mona", last_name: "Lisa", hourly_rate: 40)
+@store1.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 50)
+@store1.employees.create(first_name: "Tom", last_name: "Ford", hourly_rate: 60)
+@store2.employees.create(first_name: "Van", last_name: "Gogh", hourly_rate: 80)
+@store2.employees.create(first_name: "Coco", last_name: "Chanel", hourly_rate: 50)
+@store2.employees.create(first_name: "Louis", last_name: "Vuitton", hourly_rate: 70)
+@store2.employees.create(first_name: "Alexander", last_name: "McQueen", hourly_rate: 80)
